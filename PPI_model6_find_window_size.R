f466ccd7416683fb088c41df1cef2ad65ac83045
Re7ird <a993503077@126.com>
2023-05-11 22:56:35 +0800

    This file can run multiple models with different window size (Lag=1, with PPI, dim=8)

---
+++ PPI_model6_find_window_size.R
@@ -0,0 +1,166 @@
+#This model modified the window length of lasso estimation.
+
+#Perequisite
+library(tidyverse)
+library(glmnet)
+library(doParallel)
+registerDoParallel(detectCores()-1)
+set.seed(10)
+
+n=5
+
+
+load(url("https://github.com/zhentaoshi/Econ5821/raw/main/data_example/dataset_inf.Rdata"))
+
+
+#-----------------Start-----------------#
+st=Sys.time()
+
+#Lag=1:
+data_1=merge(ppi,X,by="month",all = T)
+data_1=data_1 |> mutate(y=lead(PPI)) |> filter(is.na(y)==F)
+
+#Lag=2:
+data_2=merge(ppi,X,by="month",all = T)
+data_2=data_2 |> mutate(y=lead(PPI,n=2)) |> filter(is.na(y)==F)
+
+#Lag=3:
+data_3=merge(ppi,X,by="month",all = T)
+data_3=data_3 |> mutate(y=lead(PPI,n=3)) |> filter(is.na(y)==F)
+
+#Train the PCA model
+pca_data=X |> select(!c("month"))
+PCA=princomp(pca_data)
+
+##------------------------------#Functions:---------------------------##
+
+
+
+pca_lasso_test2 <- function(i,data,wdl) {
+  windowlength=wdl
+  a=windowlength-1
+  b=windowlength
+  z = 160-windowlength
+  
+  j=i+a
+  k=i+b
+  y_train=data[i:j,"y"]
+  y_test=data[k,"y"]
+  x_train=data[i:j,] |> select(!c("month","y","PPI"))
+  x_train_ppi=data[i:j,"PPI"] 
+  x_test=data[k,] |> select(!c("month","y","PPI"))
+  x_test_ppi=data[k,"PPI"]
+  
+  
+  #collect Principal Components and train the lasso regression.
+  PC=predict(PCA,newdata = x_train)[,1:dim]
+  PCs=cbind(PC,x_train_ppi)
+  
+  #Calculate scores for testing
+  PC_test=predict(PCA,newdata = x_test)[,1:dim]
+  PC_test2=append(PC_test,x_test_ppi)
+  
+  #find lambda
+  cv_model <- cv.glmnet(PCs, y_train, alpha = 1,nfolds = 3)
+  best_lambda <- cv_model$lambda.min
+  
+  #display optimal lambda value
+  best_lambda
+  
+  #view coefficients of best model
+
+  pca_lasso2 <- glmnet(PCs, y_train, alpha = 1, lambda =best_lambda)
+  coef(pca_lasso2)
+  summary(pca_lasso2)
+  
+  #make a prediction for the response value of a new observation
+
+  yp=predict(pca_lasso2, s = best_lambda, newx = PC_test2)[1,1]
+  return(c(yp,y_test))
+  
+}
+
+
+rsquare_test <- function(yt,y_predicted) {
+  sst <- sum((yt - mean(yt))^2)
+  sse <- sum((y_predicted - yt)^2)
+  
+  rsq <- 1 - sse/sst
+  
+  cat("The R-square is:",rsq,"\n") 
+  return(rsq)
+}
+
+
+
+rep_test2 <- function(data,wdl){
+  z = 160-wdl
+  ybind2 <-foreach (x = 1:z,.combine = "rbind") %dopar% {
+    results=pca_lasso_test2(i = x,data,wdl)
+    y_test = results[2]
+    yp=results[1]
+    return(c(yp,y_test))
+  }
+  yp=ybind2[,1]
+  yt=ybind2[,2]
+  
+  rsquare_test(yt,y_predicted = yp)
+  
+}
+
+
+
+#---------------------Run test---------------------#
+runtest <- function(wdl){
+  windowlength=wdl
+
+
+    #------------------test 4----------------#
+    #Lag=1,method=2
+
+    rsqv=c()
+    for (t in 1:n){
+
+      rsqv=append(rsqv,rep_test2(data_1,windowlength))
+
+    }
+    rppi <- rsqv
+
+    
+    results <- c(windowlength,mean(rppi))
+
+    print("Results:")
+    cat("mean: \t",mean(rppi),"\n")
+    cat("stdev: \t",sd(rppi),"\n")
+    
+    return(results)
+} 
+
+
+
+##-----------------------------------------------------##
+
+
+dim=8
+res <-  foreach (l = c(12,24),.combine="cbind") %do%{
+  runtest(wdl = l)
+}
+
+
+# 
+# res=rbind(res_1,res_2,res_3)
+# res2=t(res)
+# 
+# 
+# pl=data.frame(res2)
+# index=1:6
+# pl=cbind(pl,index)
+
+
+
+
+ed=Sys.time()
+dr=ed-st
+
+cat("Total duration: ")
+print(dr)
